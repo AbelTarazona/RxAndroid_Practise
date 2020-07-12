64b04d049464a3ad1be5fd30aab80b16c6dfcb54
Abel <abel.tarazona@urp.edu.pe>
2020-07-12 12:31:41 -0500

    Implementacion basica y uso de Disposable

--- app/src/main/java/com/company/rxexample/MainActivity.java
+++ app/src/main/java/com/company/rxexample/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         findViewById(R.id.btnRX00Introduccion).setOnClickListener(v -> startActivity(new Intent(this, RX00IntroActivity.class)));
+        findViewById(R.id.btnRX01Disposable).setOnClickListener(v -> startActivity(new Intent(this, RX01DisposableActivity.class)));
     }
 }
--- app/src/main/java/com/company/rxexample/RX01DisposableActivity.java
+++ app/src/main/java/com/company/rxexample/RX01DisposableActivity.java
@@ -12,12 +12,15 @@ import io.reactivex.rxjava3.core.Observer;
 import io.reactivex.rxjava3.disposables.Disposable;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 
-public class RX00IntroActivity extends AppCompatActivity {
+public class RX01DisposableActivity extends AppCompatActivity {
+
+    //  Permitira desuscribir a la actividad con Rx
+    private Disposable disposable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_r_x00_intro);
+        setContentView(R.layout.activity_r_x01_disposable);
 
         //  1. Se crea Observable con Operator
         Observable<String> numerosObservable = Observable.just("1", "2", "3", "4", "5", "6", "7", "8", "9", "10");
@@ -26,25 +29,30 @@ public class RX00IntroActivity extends AppCompatActivity {
         Observer<String> numerosObserver = new Observer<String>() {
             @Override
             public void onSubscribe(@NonNull Disposable d) {
+                disposable = d;
                 //  Al hacer la conexion entre el observable y el observer
+                Log.d("TAG1", "isDispose: "+disposable.isDisposed());
                 Log.d("TAG1", "onSubscribe: HILO> "+Thread.currentThread().getName());
             }
 
             @Override
             public void onNext(@NonNull String s) {
                 //  Ira emitiendo cada valor (1..2..3..)
+                Log.d("TAG1", "isDispose: "+disposable.isDisposed());
                 Log.d("TAG1", "onNext: Numero>" + s + " []HILO " + Thread.currentThread().getName());
             }
 
             @Override
             public void onError(@NonNull Throwable e) {
                 //  En caso ocurra algun error
+                Log.d("TAG1", "isDispose: "+disposable.isDisposed());
                 Log.d("TAG1", "onError: HILO> "+Thread.currentThread().getName());
             }
 
             @Override
             public void onComplete() {
                 //  Cuando se ha completado de emitir todos los datos
+                Log.d("TAG1", "isDispose: "+disposable.isDisposed());
                 Log.d("TAG1", "onComplete: HILO> "+Thread.currentThread().getName());
             }
         };
@@ -52,8 +60,16 @@ public class RX00IntroActivity extends AppCompatActivity {
         //  3. Se hace el Subscribe y se asigna Schedulers
         numerosObservable
                 .subscribeOn(Schedulers.io()) // En que hilo queremos que se ejecute el Observable
-                .observeOn(Schedulers.io()) // En que hilo queremos se ejecute el Observer
+                .observeOn(AndroidSchedulers.mainThread()) // En que hilo queremos se ejecute el Observer
                 .subscribe(numerosObserver);
+    }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("TAG1", "isDispose: "+disposable.isDisposed());
+        disposable.dispose(); // Elimina la suscripcion entre el Observer y Observable
+        Log.d("TAG1", "isDispose: "+disposable.isDisposed());
+        Log.d("TAG1", "onDestroy: Desechamos suscripcion []HILO " + Thread.currentThread().getName());
     }
 }
